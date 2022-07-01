b58089c766c3412684ee053a3bd82cea180d54ec
Thomas Revel <thomasrevel@macbook-air-de-thomas.home>
2022-07-01 10:47:12 +0200

    Scène Menu thomas avec script transition scène.

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start a game in SceneMenu :
+    public void PlayGame()
+    {
+
+    }
+}
